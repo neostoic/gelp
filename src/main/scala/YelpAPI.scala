5df02b10b923e4f8ba89746f0790802c55e31202
Mitch Lee <mitchlee11@gmail.com>
2015-01-13 10:23:24 -0800

    Do a bit of factoring in YelpAPI.

--- src/main/scala/YelpAPI.scala
+++ src/main/scala/YelpAPI.scala
@@ -11,6 +11,13 @@ object YelpAPI {
   private val TOKEN_SECRET = ""
   private val YELP_SEARCH_URL = "http://api.yelp.com/v2/search"
 
+  private lazy val service = new ServiceBuilder()
+    .provider(classOf[TwoStepOAuth])
+    .apiKey(CONSUMER_KEY)
+    .apiSecret(CONSUMER_SECRET)
+    .build()
+  private lazy val accessToken = new Token(TOKEN, TOKEN_SECRET)
+
   case class Coordinate(latitude: Double, longitude: Double)
   case class Location(postal_code: String, address: List[String], coordinate: Coordinate)
   case class Business(id: String, name: String, rating: Double, review_count: BigInt, phone: Option[String], location: Location) {
@@ -26,27 +33,28 @@ object YelpAPI {
 
   def runYelpSearch() {
     println("Let's get this Yelp party started!")
-    val service = new ServiceBuilder()
-      .provider(classOf[TwoStepOAuth])
-      .apiKey(CONSUMER_KEY)
-      .apiSecret(CONSUMER_SECRET)
-      .build()
 
-    val accessToken = new Token(TOKEN, TOKEN_SECRET)
-    val request = new OAuthRequest(Verb.GET, s"$YELP_SEARCH_URL?$yelpQueryParams")
-
-    println(s"Querying ${ request.getCompleteUrl } ...")
-    service.signRequest(accessToken, request)
-    val response = request.send()
-    println(if (response.isSuccessful) "Success!" else "Failure!")
-
-    val jsonResp = parse(response.getBody)
+    val jsonResp = send(s"$YELP_SEARCH_URL?$yelpQueryParams")
 
     implicit val formats = DefaultFormats
     val allBusinessResults = (jsonResp \ "businesses").extract[List[Business]]
 
     allBusinessResults.map(_.toDisplayString).foreach(println)
+  }
+
+  def send(requestURL: String) = {
+    val request = new OAuthRequest(Verb.GET, requestURL)
+
+    println(s"\nQuerying ${ request.getCompleteUrl } ...")
+
+    service.signRequest(accessToken, request)
+    val rawResp = request.send()
+    val jsonResp = parse(rawResp.getBody)
+
+    println(if (rawResp.isSuccessful) "Success!" else "Failure!")
     println(pretty(render(jsonResp)))
+
+    jsonResp
   }
 
   def yelpQueryParams = Map(
