e95b24682b8cd3968c1449ca204d493342cfd2ee
Mitch Lee <mitchlee11@gmail.com>
2015-01-13 10:23:05 -0800

    Fetch and parse google places details for each business.

--- src/main/scala/GooglePlacesAPI.scala
+++ src/main/scala/GooglePlacesAPI.scala
@@ -1,5 +1,6 @@
 import dispatch._
 import Defaults._
+import org.json4s.DefaultFormats
 import org.json4s.native.JsonMethods._
 
 object GooglePlacesAPI {
@@ -9,22 +10,46 @@ object GooglePlacesAPI {
   private val NEARBY_SEARCH_URL = url("https://maps.googleapis.com/maps/api/place/nearbysearch/json")
   private val PLACE_DETAILS_URL = url("https://maps.googleapis.com/maps/api/place/details/json")
 
+  case class PlaceID(place_id: String)
+  case class Coordinate(lat: Double, lng: Double)
+  case class Geometry(location: Coordinate)
+  case class Business(place_id: String, name: String, rating: Double, user_ratings_total: BigInt, price_level: BigInt, formatted_address: String, website: Option[String], formatted_phone_number: Option[String], geometry: Geometry)  {
+    def toDisplayString =
+      s"""
+         |ID: $place_id
+         |Name: $name
+         |Rating: $rating
+         |Reviews: $user_ratings_total
+         |Phone: ${formatted_phone_number.getOrElse("N/A")}
+       """.stripMargin
+  }
+
   def runGoogleSearch() {
     println("Let's get this Google party started!")
 
-    val request = searchURL
-    val response = Http(request OK as.String)
+    val response = send(searchRequest)
 
-    val jsonResp = parse(s"""${ response() }""")
+    implicit val formats = DefaultFormats
+    val placeIDs = (response \ "results").extract[List[PlaceID]]
+    val placeDetails = placeIDs.map(id => send(detailsRequest(id)))
+    val businesses = placeDetails.map(placeDetail => (placeDetail \ "result").extract[Business])
 
-    // JSON Documentation: https://github.com/json4s/json4s
-    println(s"Status is: ${ (jsonResp \ "status").values }")
-    println(pretty(render(jsonResp)))
+    businesses.map(_.toDisplayString).foreach(println)
     sys.exit()
   }
 
+  def send(request: Req) = {
+    val rawResp = Http(request OK as.String)
+    val jsonResp = parse(s"""${ rawResp() }""")
+
+    println(s"\nStatus is: ${ (jsonResp \ "status").values }")
+    println(pretty(render(jsonResp)))
+
+    jsonResp
+  }
+
   // Documentation: https://developers.google.com/places/documentation/search#PlaceSearchRequests
-  def searchURL = NEARBY_SEARCH_URL <<? searchQueryParams
+  def searchRequest = NEARBY_SEARCH_URL <<? searchQueryParams
   def searchQueryParams = Map(
     "key" -> SERVER_API_KEY,
     "location" -> "37.776472,-122.437833", // The Mill
@@ -33,10 +58,10 @@ object GooglePlacesAPI {
   )
 
   // Documentation: https://developers.google.com/places/documentation/details
-  def detailsURL(placeID: String) = PLACE_DETAILS_URL <<? detailsQueryParams(placeID)
-  def detailsQueryParams(placeID: String) = Map(
+  def detailsRequest(placeID: PlaceID) = PLACE_DETAILS_URL <<? detailsQueryParams(placeID)
+  def detailsQueryParams(placeID: PlaceID) = Map(
     "key" -> SERVER_API_KEY,
-    "placeid" -> placeID
+    "placeid" -> placeID.place_id
   )
 
 }
